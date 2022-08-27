935e58423fcaf1aa60046567426943c7d38975db
unknown <yogeshmore044@gmail.com>
2022-08-27 11:20:24 +0530

    Operators

--- project-1/datatypes.js
+++ project-1/datatypes.js
@@ -21,9 +21,9 @@
 // let car2 = "Audi";
 // let car3 = "TATA";
 
-// let car = ["BMW", "Audi", "TATA"]; // index = 0
+// let car = ["BMW", 12, 1.52,"Audi", "TATA"]; // index = 0
 
-// console.log(car[1]);
+// console.log(car[0]);
 // console.log(car[2]);
 
 let avi = ["BMW", 11, 1.015, {
@@ -31,11 +31,13 @@ let avi = ["BMW", 11, 1.015, {
     lastName: "Ldarwin",
     age: 50,
     childrenNames: [],
-    eligible: () => {
-        console.log("Age", this);
+    eligible: function () {
+        console.log("Age", this.name);
         // console.log("eligible");
     }
 }]; // index = 0
+
+const squareRoot = (adhhaarId) => { }
 
 // console.log(avi[3].age);
 avi[3].eligible();
